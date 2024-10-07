e6c4d30a5d0656cb6053614d88aa22f63194a5f8
RodrigoDelgado005 <162932332+RodrigoDelgado005@users.noreply.github.com>
2024-10-07 02:09:02 -0300

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,25 +1,31 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
+/* @tutinfo Import <CODEIonicons</CODE> icon set.*/
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
+      /* @tutinfo There are many <CODE>screenOptions</CODE> we can use to customize the tab bar. We're changing the active tab color here to custom value which we will also use later in our app. */
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
+        tabBarActiveTintColor: '#ffd33d',
+        headerStyle: {
+          backgroundColor: '#25292e',
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#fff',
+        tabBarStyle: {
+        backgroundColor: '#25292e',
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            /* @tutinfo The <CODE>focused</CODE> param allows us to change a tab's icon and label behavior when it is active and inactive.*/
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
           ),
         }}
       />
@@ -28,7 +34,17 @@ export default function TabLayout() {
         options={{
           title: 'Explore',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <Ionicons name={focused ? 'code-slash' : 'code-slash-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="contact"
+        options={{
+          title: 'Contact',
+          tabBarIcon: ({ color, focused }) => (
+            /* @tutinfo */
+            <Ionicons name={focused ? 'call' : 'call-outline'} color={color} size={24}/>
           ),
         }}
       />
---
+++ app/(tabs)/contact.tsx
@@ -0,0 +1,103 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, Platform } from 'react-native';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { View, Text, TouchableOpacity, TextInput, Alert } from 'react-native';
+import React, { useState } from 'react';
+
+
+export default function Contact() {
+  const [nombre, setNombre] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [mensaje, setMensaje] = useState('');
+
+  const envioMensaje = () => {
+    Alert.alert("Gracias por contactarnos, te responderemos lo antes posible");
+
+    setNombre('');
+    setCorreo('');
+    setMensaje('');
+
+};
+  return (
+    
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerImage={<Ionicons size={310} name="call-outline" style={styles.headerImage} />}
+    >
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Contáctanos</ThemedText>
+      </ThemedView>
+
+      <ThemedText>¡Estamos para ayudarte! Contáctanos a través del siguiente medio:</ThemedText>
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre"
+        value={nombre}
+        onChangeText={setNombre}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Correo electrónico"
+        value={correo}
+        onChangeText={setCorreo}
+      />
+
+      <TextInput
+        style={[styles.input, styles.textArea]}
+        placeholder="Escribe tu mensaje"
+        value={mensaje}
+        onChangeText={setMensaje}
+        multiline={true}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={envioMensaje}>
+        <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+
+    backgroundColor: '#FFF',
+    color: '#535151',
+  },
+  textArea: {
+    height: 95,
+    textAlignVertical: 'top',
+    paddingVertical: 10,
+  },
+  button: {
+    backgroundColor: '#4F8EF7',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    width:'50%',
+    alignSelf:'center'
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+});
